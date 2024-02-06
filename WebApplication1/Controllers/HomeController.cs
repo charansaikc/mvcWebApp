b2529ffc3ff6c505365f9eb5b1298b78ea9ef10b
ADMIN <ADMIN@DESKTOP-3G1VJ8B>
2024-02-06 14:34:50 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            student student = new student();
+            student.name = "charan sai";
+            student.city = "ballari";
+
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class student
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public string city { get; set; }
+    }
+}
